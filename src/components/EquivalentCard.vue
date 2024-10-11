edbb275933d6812657316be7dcf9e03fd0295176
Francisco Amador <isco.amaador@gmail.com>
2024-10-11 11:22:13 -0600

    refactor: sort items

--- src/components/EquivalentCard.vue
+++ src/components/EquivalentCard.vue
@@ -23,25 +23,25 @@ const {} = defineProps({
         </tr>
       </thead>
       <tbody>
-        <FoodCardItem header="Frutas" :value="equivalents.fruits" />
-        <FoodCardItem header="Grasas sin proteínas" :value="equivalents.fats_without_pretein" />
         <FoodCardItem header="Verduras" :value="equivalents.vegetables" />
-        <FoodCardItem header="Libres en energía" :value="equivalents.free_energy" />
+        <FoodCardItem header="Frutas" :value="equivalents.fruits" />
+        <FoodCardItem header="Cereales S/G" :value="equivalents.sg_cereals" />
+        <FoodCardItem header="Cereales C/G" :value="equivalents.cg_cereals" />
+        <FoodCardItem header="Leguminosas" :value="equivalents.legumes" />
         <FoodCardItem header="AOA MBAG" :value="equivalents.aoa_mbag" />
+        <FoodCardItem header="AOA BAG" :value="equivalents.aoa_bag" />
+        <FoodCardItem header="AOA MAG" :value="equivalents.aoa_mag" />
+        <FoodCardItem header="AOA AAG" :value="equivalents.aoa_aag" />
+        <FoodCardItem header="Leche descremada" :value="equivalents.skim_milk" />
+        <FoodCardItem header="Leche semidescremada" :value="equivalents.semi_skimmed_milk" />
+        <FoodCardItem header="Leche entera" :value="equivalents.milk" />
         <FoodCardItem header="Leche con azúcar" :value="equivalents.milk_with_sugar" />
+        <FoodCardItem header="Grasas sin proteínas" :value="equivalents.fats_without_pretein" />
+        <FoodCardItem header="Grasas con proteínas" :value="equivalents.fats_with_protein" />
         <FoodCardItem header="Azucares sin grasa" :value="equivalents.fat_free_sugars" />
         <FoodCardItem header="Azucares con grasa" :value="equivalents.sugars_with_fat" />
-        <FoodCardItem header="Grasas con proteínas" :value="equivalents.fats_with_protein" />
+        <FoodCardItem header="Libres en energía" :value="equivalents.free_energy" />
         <FoodCardItem header="Alcohol" :value="equivalents.alcohol" />
-        <FoodCardItem header="AOA BAG" :value="equivalents.aoa_bag" />
-        <FoodCardItem header="AOA AAG" :value="equivalents.aoa_aag" />
-        <FoodCardItem header="Cereales S/G" :value="equivalents.sg_cereals" />
-        <FoodCardItem header="Leguminosas" :value="equivalents.legumes" />
-        <FoodCardItem header="Cereales C/G" :value="equivalents.cg_cereals" />
-        <FoodCardItem header="AOA MAG" :value="equivalents.aoa_mag" />
-        <FoodCardItem header="Leche descremada" :value="equivalents.skim_milk" />
-        <FoodCardItem header="Leche entera" :value="equivalents.milk" />
-        <FoodCardItem header="Leche semidescremada" :value="equivalents.semi_skimmed_milk" />
       </tbody>
     </table>
   </div>
