b3915b25c5619fee12b3a7f5d476df2c92a6e648
Francisco Amador <isco.amaador@gmail.com>
2025-01-23 16:27:07 -0600

    feat: integrate VueApexCharts for enhanced charting capabilities

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { router } from './router'
+import VueApexCharts from 'vue3-apexcharts';
 import './style.css'
 import App from './App.vue'
 
 createApp(App)
   .use(router)
   .use(createPinia())
+  .use(VueApexCharts)
   .mount('#app')
