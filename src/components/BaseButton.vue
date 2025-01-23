d5819313f1f88c4f511c4053f8ad404ee1091c11
Francisco Amador <isco.amaador@gmail.com>
2025-01-23 16:27:56 -0600

    feat: update BaseButton component with custom color for vc-lilac theme

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,6 +1,6 @@
 <template>
   <button type="button"
-    class="px-5 py-2.5 text-sm font-medium text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+    class="px-5 py-2.5 text-sm font-medium text-white inline-flex items-center bg-[#C8A2C8] hover:bg-[#C8A2C8]/[.5] focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-center dark:bg-[#C8A2C8] dark:hover:bg-[#C8A2C8]/[.5] dark:focus:ring-blue-800">
     <slot></slot>
   </button>
 </template>
