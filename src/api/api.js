42916b9bf4b7fe6f48336a4c7601c23ef222d906
Francisco Amador <isco.amaador@gmail.com>
2025-08-20 19:48:09 -0600

    feat: actualizar la URL base de la API a la versión en producción en api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: "http://localhost:3010"
-    // baseURL: "https://nutristern-api.vercel.app"
+    baseURL: "https://nutristern-api.vercel.app"
 })
 
 export default api
