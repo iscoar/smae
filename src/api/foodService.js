02abd54eea7b00a87c4c3db9eb74ff7a972e2067
Francisco Amador <isco.amaador@gmail.com>
2025-08-20 18:54:31 -0600

    feat: agregar propiedades alimenticias desde food_props.json y exportarlas en foodService.js

--- src/api/foodService.js
+++ src/api/foodService.js
@@ -1,7 +1,10 @@
 import dataHeaders from './data/headers.json'
+import foodProperties from './data/food_props.json'
 import api from './api'
 
 export const headers = dataHeaders
+
+export const foodProps = foodProperties
 
 export const getAllFoodByName = (name) => api.get(`/food?name=${name}`)
 
