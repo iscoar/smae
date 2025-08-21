e66e07f48f6f52e13b040b14649c64dff12bae26
Francisco Amador <isco.amaador@gmail.com>
2025-08-20 18:57:35 -0600

    feat: implementar renderizado dinámico de FoodCardItem utilizando un array de campos en EquivalentCard

--- src/components/EquivalentCard.vue
+++ src/components/EquivalentCard.vue
@@ -1,7 +1,7 @@
 <script setup>
 import FoodCardItem from './FoodCardItem.vue';
 
-const {} = defineProps({
+const { equivalents, color, fields } = defineProps({
   equivalents: {
     type: Object,
     default: () => ({})
@@ -9,7 +9,11 @@ const {} = defineProps({
   color: {
     type: String,
     default: 'bg-[#76d7c4]'
-  }
+  },
+  fields: {
+    type: Array,
+    default: () => []
+  },
 })
 </script>
 
@@ -27,25 +31,13 @@ const {} = defineProps({
         </tr>
       </thead>
       <tbody>
-        <FoodCardItem header="Verduras" :value="equivalents.vegetables" :color="color" />
-        <FoodCardItem header="Frutas" :value="equivalents.fruits" :color="color" />
-        <FoodCardItem header="Cereales S/G" :value="equivalents.sg_cereals" :color="color" />
-        <FoodCardItem header="Cereales C/G" :value="equivalents.cg_cereals" :color="color" />
-        <FoodCardItem header="Leguminosas" :value="equivalents.legumes" :color="color" />
-        <FoodCardItem header="AOA MBAG" :value="equivalents.aoa_mbag" :color="color" />
-        <FoodCardItem header="AOA BAG" :value="equivalents.aoa_bag" :color="color" />
-        <FoodCardItem header="AOA MAG" :value="equivalents.aoa_mag" :color="color" />
-        <FoodCardItem header="AOA AAG" :value="equivalents.aoa_aag" :color="color" />
-        <FoodCardItem header="Leche descremada" :value="equivalents.skim_milk" :color="color" />
-        <FoodCardItem header="Leche semidescremada" :value="equivalents.semi_skimmed_milk" :color="color" />
-        <FoodCardItem header="Leche entera" :value="equivalents.milk" :color="color" />
-        <FoodCardItem header="Leche con azúcar" :value="equivalents.milk_with_sugar" :color="color" />
-        <FoodCardItem header="Grasas sin proteínas" :value="equivalents.fats_without_pretein" :color="color" />
-        <FoodCardItem header="Grasas con proteínas" :value="equivalents.fats_with_protein" :color="color" />
-        <FoodCardItem header="Azucares sin grasa" :value="equivalents.fat_free_sugars" :color="color" />
-        <FoodCardItem header="Azucares con grasa" :value="equivalents.sugars_with_fat" :color="color" />
-        <FoodCardItem header="Libres en energía" :value="equivalents.free_energy" :color="color" />
-        <FoodCardItem header="Alcohol" :value="equivalents.alcohol" :color="color" />
+        <FoodCardItem
+          v-for="field in fields"
+          :key="field.key"
+          :header="field.value"
+          :value="equivalents[field.key]"
+          :color="color"
+        />
       </tbody>
     </table>
   </div>
